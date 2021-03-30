a7eb8976b610e438757fc6e827ebc62b2e1711af
Gry Nagel <desoto@DESKTOP-KQR34FP.localdomain>
2021-03-30 18:19:20 +0200

    Add keystone backend

---
+++ backend/keystone.ts
@@ -0,0 +1,60 @@
+import "dotenv/config";
+import { config, createSchema } from "@keystone-next/keystone/schema";
+import { createAuth } from "@keystone-next/auth";
+import {
+  withItemData,
+  statelessSessions,
+} from "@keystone-next/keystone/session";
+
+import { User } from "./schemas/User";
+
+const databaseUrl =
+  process.env.DATABASE_URL || "mongodb://localhost/keystone-sick-fits-tutorial";
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360, // How long they stay signed in?
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: "User",
+  identityField: "email",
+  secretField: "password",
+  initFirstItem: {
+    fields: ["name", "email", "password"],
+  },
+});
+
+export default withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: "mongoose",
+      url: databaseUrl,
+      async onConnect(keystone) {
+        if (process.argv.includes("--seed-data")) {
+          console.log("Here we can add some seed data if we want");
+          //   await insertSeedData(keystone);
+        }
+      },
+    },
+    lists: createSchema({
+      User,
+    }),
+    ui: {
+      // Show the UI only for people who pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query
+      User: `id name email`,
+    }),
+  })
+);
